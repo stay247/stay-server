b99e659c20e90a07dc17f71f2935e322885d8b99
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-01-25 23:34:18 +0900

    firstCommit

---
+++ src/main/java/com/stayserver/stayserver/StayServerApplication.java
@@ -0,0 +1,13 @@
+package com.stayserver.stayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayServerApplication.class, args);
+	}
+
+}
